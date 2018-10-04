9e145b9d623f27eb7162fbe092f33cd9e23f94c9
Peter Shershov <peter.shershov@kenshoo.com>
2018-10-05 02:52:52 +0300

    small refactor

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const { parse } = require('path')
-const { execSync } = require('child_process')
+const { parse } = require("path");
+const { execSync } = require("child_process");
 
 class webpackNoodlePlugin {
-  constructor({filename, command}) {
+  constructor({ filename, command }) {
     this.filename = filename;
     this.command = command;
   }
 
   apply(compiler) {
-    compiler.plugin('invalid', (filePath) => {
+    compiler.plugin("invalid", filePath => {
       if (parse(filePath).base.indexOf(this.filename) !== -1) {
-        execSync(this.command, { stdio: 'inherit', shell: true })
+        execSync(this.command, { stdio: "inherit", shell: true });
       }
-    })
+    });
   }
 }
 
